68fb3412ad2684f797390bc15165cdd2883d56fb
Arthur Lopes <arthurlopr12@gmail.com>
2025-07-08 20:32:46 -0300

    feat: initial commit

---
+++ src/main/kotlin/codeflix/codeflixconverter/CodeflixConverterApplication.kt
@@ -0,0 +1,11 @@
+package codeflix.codeflixconverter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodeflixConverterApplication
+
+fun main(args: Array<String>) {
+    runApplication<CodeflixConverterApplication>(*args)
+}
---
+++ src/test/kotlin/codeflix/codeflixconverter/CodeflixConverterApplicationTests.kt
@@ -0,0 +1,13 @@
+package codeflix.codeflixconverter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CodeflixConverterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
